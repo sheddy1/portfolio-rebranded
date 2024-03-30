c77ced7afb44838b6a27f17ac035b087def64c34
sheddy1 <shadrachgodwin@gmail.com>
2024-03-30 21:08:34 +0100

    header chechkd

--- portfolio/resources/views/welcome.blade.php
+++ portfolio/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="icon" href="{{ URL('public/images/logo1.png') }}">
+    <link rel="icon" href="{{ asset('images/logo1.png') }}">
     <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
